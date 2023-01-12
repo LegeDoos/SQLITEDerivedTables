e622d5e4f084eeecaafc53bd57b23f218caff73e
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-01-12 11:18:13 +0100

    Add project files.

---
+++ Classes.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDerivedTables
+{
+    public class Animal
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Cattle : Animal
+    {
+        public int HornsCount { get; set; }
+    }
+
+    public class Fish : Animal
+    {
+        public int FlippersCount { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SQLDerivedTables;
+using SQLite;
+
+Console.WriteLine("Test opslag derived tables");
+
+
+using (var conn = new SQLiteConnection("database.sqlite"))
+{
+    // create table
+    conn.CreateTable<Cattle>();
+    // insert data with autoincrement
+    conn.Insert(new Cattle() { Name = "Cattle 1", HornsCount = 10 });
+    conn.Insert(new Cattle() { Name = "Cattle 2", HornsCount = 20 });
+    conn.Insert(new Cattle() { Name = "Cattle 3", HornsCount = 30 });
+
+    foreach (var item in conn.Table<Cattle>().ToList())
+    {
+        Console.WriteLine($"Cattle Id = {item.Id}; Name = {item.Name}; Hornscount = {item.HornsCount}");
+    }
+
+    // create other table
+    conn.CreateTable<Fish>();
+    // insert data with autoincrement
+    conn.Insert(new Fish() { Name = "Fish 1", FlippersCount = 6 });
+    conn.Insert(new Fish() { Name = "Fish 2", FlippersCount = 7 });
+    conn.Insert(new Fish() { Name = "Fish 3", FlippersCount = 8 });
+
+    try
+    {
+        foreach (var item in conn.Table<Animal>().ToList())
+        {
+            Console.WriteLine($"Animal Id = {item.Id}; Name = {item.Name}; ");
+        }
+
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Tabel animal bestaat niet dus het worden echt aparte tabellen!");
+    }
+
+
+
+}
+
+
